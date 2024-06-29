4989d6467a8b6df9a5cd5b6c029c0ab0c814defb
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-06-30 01:16:26 +0530

    format code using prettier-plugin-tailwindcss

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,10 @@
 function Header() {
   return (
-    <header className="flex w-full md:justify-start justify-center py-4 md:px-20 md:py-8">
+    <header className="flex w-full justify-center py-4 md:justify-start md:px-20 md:py-8">
       {/* logo */}
-      <div className="flex gap-2 md:gap-4 items-center">
-        <img
-          src="/logo.svg"
-          alt="Coditor logo"
-          height="64"
-          width="64"
-        />
-        <img
-          src="/text_logo.svg"
-          alt="Coditor logo"
-          height="64"
-          width="256"
-        />
+      <div className="flex items-center gap-2 md:gap-4">
+        <img src="/logo.svg" alt="Coditor logo" height="64" width="64" />
+        <img src="/text_logo.svg" alt="Coditor logo" height="64" width="256" />
       </div>
     </header>
   );
