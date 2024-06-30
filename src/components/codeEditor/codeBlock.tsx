fc9b4c594a9097476ac672001d1fd722579fff69
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-07-01 03:23:40 +0530

    rename main code editor component to CodeBlock
    required functionality achieved

--- src/components/codeEditor/codeBlock.tsx
+++ src/components/codeEditor/codeBlock.tsx
@@ -1,21 +1,26 @@
 import { Highlight, themes } from "prism-react-renderer";
 import { useRef, useState } from "react";
+import { TLang, TTheme } from "../../types";
 
-function CodeBlock() {
+type TProps = {
+  prismTheme: TTheme;
+  prismLang: TLang;
+};
+
+function CodeBlock({ prismTheme, prismLang }: TProps) {
   const [codeText, setCodeText] = useState("");
   const codeHighlightRef = useRef<HTMLPreElement>(null);
   const handleCodeTextUpdate = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const code = e.target.value;
-    setCodeText(code);
+    setCodeText(e.target.value);
   };
   // NOTE: For responsive ness
   // Need to scroll textarea to update padding and margins after resizing browser
   // HACK:
   // textarea absolute positioning and calculated margin+padding to place caret at end of line
   return (
-    <div className="max-w-9xl container relative mx-auto my-auto mt-10 h-[450px] overflow-hidden px-4 md:mt-20 md:h-[550px]">
+    <div className="relative h-[450px] overflow-hidden px-4 md:h-[550px]">
       <textarea
-        className="absolute bottom-0 left-[56px] right-4 top-0 select-none resize-none overflow-y-auto break-all bg-transparent py-2 pe-4 font-mono tracking-tight text-transparent caret-white focus:outline-none"
+        className="absolute bottom-0 left-[56px] right-4 top-0 select-none resize-none overflow-y-auto break-all bg-transparent py-2 pe-4 font-mono tracking-tight text-transparent caret-green-400 focus:outline-none"
         spellCheck={false}
         name="codeBlock"
         value={codeText}
@@ -24,9 +29,13 @@ function CodeBlock() {
           if (!codeHighlightRef.current) return;
           codeHighlightRef.current.scroll({ top: e.currentTarget.scrollTop });
         }}
-      ></textarea>
+      />
       <div className="select-none bg-auto tracking-tight">
-        <Highlight theme={themes.jettwaveDark} code={codeText} language="tsx">
+        <Highlight
+          theme={themes[prismTheme]}
+          code={codeText}
+          language={prismLang.lang}
+        >
           {({ style, tokens, getLineProps, getTokenProps }) => (
             <pre
               ref={codeHighlightRef}
@@ -39,7 +48,7 @@ function CodeBlock() {
                   {...getLineProps({ line })}
                   className="grid grid-cols-[40px_1fr]"
                 >
-                  <span className="place-self-center">{i + 1}</span>
+                  <span className="place-self-center self-start">{i + 1}</span>
                   <div className="text-wrap pe-4">
                     {line.map((token, key) => (
                       <span
