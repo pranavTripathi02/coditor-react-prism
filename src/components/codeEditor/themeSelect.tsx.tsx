68b931604923d27abb149117157c8cfa7e5016fa
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-07-01 03:26:21 +0530

    add component for PrismJS theme selection

---
+++ src/components/codeEditor/themeSelect.tsx.tsx
@@ -0,0 +1,27 @@
+import { TTheme } from "../../types";
+import { themes } from "prism-react-renderer";
+import { Option, Select } from "../primitives";
+
+type TProps = {
+  theme: TTheme;
+  updateTheme: (newTheme: TTheme) => void;
+};
+
+const PrismThemeSelect = ({ theme, updateTheme }: TProps) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selTheme = e.target.value as TTheme;
+    updateTheme(selTheme);
+  };
+  return (
+    <label className="flex items-center gap-2">
+      <span>Theme: </span>
+      <Select onChange={handleChange} defaultValue={theme}>
+        {Object.keys(themes).map((theme) => (
+          <Option value={theme}>{theme}</Option>
+        ))}
+      </Select>
+    </label>
+  );
+};
+
+export default PrismThemeSelect;
