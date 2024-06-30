efb27ded8ad342a3eea6fb87d75fc61f4f90f0d8
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-07-01 04:18:10 +0530

    add hello world code for different prog langs

--- src/types/prismLanguages.ts
+++ src/types/prismLanguages.ts
@@ -1,55 +1,123 @@
 export const langList = [
   {
     lang: "jsx",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    export default function MyApp() {
+      return (
+        <div>
+          <h1>Hello World!</h1>
+        </div>
+      );
+    }
+    `,
   },
   {
     lang: "tsx",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    export default function MyApp() {
+      return (
+        <div>
+          <h1>Welcome to my app</h1>
+          <MyButton />
+        </div>
+      );
+    }
+    `,
   },
   {
     lang: "swift",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    import Swift
+
+    print("Hello, World!")
+    `,
   },
   {
     lang: "kotlin",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    fun main() {
+        println("Hello, World!")
+    }
+`,
   },
   {
     lang: "objectivec",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    #import <Foundation/Foundation.h>
+
+    int main(int argc, const char * argv[]) {
+        @autoreleasepool {
+            // insert code here...
+            NSLog(@"Hello, World!");
+        }
+        return 0;
+    }
+    `,
   },
   {
     lang: "js-extras",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    console.log("hello world!")
+    `,
   },
   {
     lang: "reason",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    String string;
+    string = "hello world";
+    `,
   },
   {
     lang: "rust",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    fn main() {
+        println!("Hello, World!");
+    }
+    `,
   },
   {
     lang: "graphql",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    {
+      "data": {
+        "hello": "Hello world!"
+      }
+    }
+    `,
   },
   {
     lang: "yaml",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    hello: world
+    `,
   },
   {
     lang: "go",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    package main
+
+    import "fmt"
+
+    func main() {
+        fmt.Println("Hello, World!")
+    }
+    `,
   },
   {
     lang: "cpp",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    #include <iostream>
+    int main() {
+        std::cout << "Hello World";
+        return 0;
+    }
+`,
   },
   {
     lang: "markdown",
-    defaultCodeExample: "",
+    defaultCodeExample: `
+    # Hello World
+    `,
   },
 ] as const;
 
