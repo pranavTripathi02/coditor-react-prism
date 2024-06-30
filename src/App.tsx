d3ef3b4c2cc0a8d1d5049891a24ddba31a68e83d
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-07-01 03:30:40 +0530

    update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Footer from "./components/footer";
+import CodeEditor from "./components/codeEditor";
+import Header from "./components/header";
+
 function App() {
   return (
     <>
-      <div className="text-red-800">Hi</div>
+      <div className="flex min-h-dvh flex-col justify-between gap-12 border-4 border-green-500">
+        <Header />
+        <CodeEditor />
+        <Footer />
+      </div>
     </>
   );
 }
